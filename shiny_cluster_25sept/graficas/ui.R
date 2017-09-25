f3635fd37f8d71ad8afcea8604b003746b2fb224
isabelmillan <anaisabelmillan9@gmail.com>
2017-09-25 12:11:09 -0500

    arreglando filtros graficos y colores

---
+++ shiny_cluster_25sept/graficas/ui.R
@@ -0,0 +1,223 @@
+### Análisis gráfico
+set.seed(314159)
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(scales)
+library(DT)
+library(quantreg)
+library(Hmisc)
+source("utiles.R")
+options(shiny.maxRequestSize = 6000*1024^2) # 6000 MB/ 6 GB
+
+
+shinyUI(fluidPage(
+  #titlePanel("Gráficas"),
+  uiOutput("default"),
+  sidebarLayout(
+    sidebarPanel(
+      tabsetPanel(
+        tabPanel("Inputs", 
+                 #tagList(
+                 # singleton(tags$head(tags$script(src='//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js',
+                 #                                 type='text/javascript'))),
+                 # singleton(tags$head(tags$script(src='//cdn.datatables.net/tabletools/2.2.4/js/dataTables.tableTools.min.js',
+                 #                                 type='text/javascript'))),
+                 #singleton(tags$head(tags$script(src='//cdn.datatables.net/colreorder/1.1.3/js/dataTables.colReorder.min.js',type='text/javascript'))),
+                 #singleton(tags$head(tags$script(src='colvis.js',type='text/javascript'))),
+                 #singleton(tags$head(tags$script(src='//cdn.datatables.net/tabletools/2.2.4/js/ZeroClipboard.min.js',type='text/javascript'))),
+                 #singleton(tags$head(tags$link(href='//cdn.datatables.net/tabletools/2.2.4/css/dataTables.tableTools.css',rel='stylesheet',type='text/css'))),
+                 #singleton(tags$script(HTML("if (window.innerHeight < 400) alert('Screen too small');"))),
+                 #tags$head( tags$style(HTML("
+                 #                            .cvclear{
+                 #                            text-align:right}"))) 
+                 #),
+                 
+                 
+                 fileInput("datafile", "Cargar datos de un archivo csv", multiple = FALSE, accept = c("csv")),
+                 
+                 uiOutput("ycol"),
+                 uiOutput("slider2"),
+                 uiOutput("xcol"),
+                 uiOutput("slider"),
+                 # uiOutput("maxlevels"),
+                 
+                 tabPanel("Filtros", 
+                          #h4("Filtros"),
+                          uiOutput("filtervar1"),
+                          uiOutput("filtervar1values"),
+                          uiOutput("filtervar2"),
+                          uiOutput("filtervar2values")
+                 )
+                 
+                 #hr()
+        ), # tabpanel
+        
+        tabPanel("Ajustes de la gráfica",
+                 textInput('ylab', 'Nombre del eje Y', value = "") ,
+                 textInput('xlab', 'Nombre del eje X', value = "") ,
+                 selectInput('backgroundcol', label ='Color del fondo',
+                             choices=c("Gris" ="gray97","Blanco"="white","Gris Oscuro"="grey90", "Negro"="black"),
+                             multiple=FALSE, selectize=TRUE,selected="gray97"),
+                 h4("Líneas de referencia"),
+                 checkboxInput('identityline', 'Línea identidad')    ,   
+                 checkboxInput('horizontalzero', 'Línea horizontal en cero'),
+                 checkboxInput('customline1', 'Línea vertical interactiva'),
+                 conditionalPanel(
+                   condition = "input.customline1" , 
+                   numericInput("vline",label = "",value = 1) )
+                 ,
+                 checkboxInput('customline2', 'Línea horizontal interactiva'),
+                 conditionalPanel(
+                   condition = "input.customline2" , 
+                   numericInput("hline",label = "",value = 1) )
+        )
+      )
+    ), #sidebarPanel
+    mainPanel(
+      h3("Análisis gráfico"),
+      tabsetPanel(
+        tabPanel("Datos",
+                 h4("En caso de querer usar una muestra random de la base:"),
+                 uiOutput("muestra_random"),
+                 tableOutput("vista"),
+                 value=1
+        ),
+        tabPanel("Gráfica de dispersión"  , 
+                 plotOutput('plot',  width = "100%" ,click = "plot_click",
+                            hover = hoverOpts(id = "plot_hover", delayType = "throttle"),
+                            brush = brushOpts(id = "plot_brush")),
+                 
+                 #verbatimTextOutput("plotinfo"),
+                 hr(),
+                 uiOutput("clickheader"),
+                 tableOutput("plot_clickedpoints"),
+                 uiOutput("brushheader"),
+                 tableOutput("plot_brushedpoints"),
+                 
+                 
+                 #actionButton("plotButton", "Update Plot"),
+                 
+                 uiOutput("optionsmenu") ,
+                 
+                 conditionalPanel(
+                   condition = "input.showplottypes" , 
+                   
+                   fluidRow(
+                     
+                     column (12, hr()),
+                     column (4,
+                             radioButtons("Points", "Puntos/Jitter:",
+                                          c("Puntos" = "Points",
+                                            "Jitter" = "Jitter",
+                                            "Ninguno" = "None")),
+                             conditionalPanel( " input.Points== 'Points' ",
+                                               sliderInput("pointstransparency", "Transparencia de puntos", min=0, max=1, value=c(0.5),step=0.01))
+                     ),
+                     column(4,
+                            conditionalPanel( " input.Points== 'Points' ",
+                                              sliderInput("pointsizes", "Tamaño del punto", min=0, max=4, value=c(1),step=0.1),
+                                              numericInput('pointtypes','Tipo de punto',16, min = 1, max = 25),
+                                              selectInput('colorpunto', 'Color', choices=colors(),multiple=FALSE, selectize=TRUE,selected="black")
+                            )),
+                     
+                     column (4,
+                             
+                             radioButtons("line", "Líneas",
+                                          c("Lineas" = "Lines",
+                                            "Ninguno" = "None"),selected="None"),
+                             conditionalPanel( " input.line== 'Lines' ",
+                                               sliderInput("linestransparency", "Transparencia de líneas", min=0, max=1, value=c(0.5),step=0.01)
+                                               
+                                               
+                             )
+                             
+                     ),
+                     column(4, conditionalPanel( " input.line== 'Lines' ",
+                                                 sliderInput("linesize", "Tamaño de línea", min=0, max=4, value=c(1),step=0.1),
+                                                 selectInput('linetypes','Tipo de línea',c("solid","dotted"))
+                                                 
+                     ) )
+                     
+                   )#fluidrow
+                 ) ,
+                 conditionalPanel(
+                   condition = "input.showfacets" , 
+                   fluidRow(
+                     column (12, hr()),
+                     column (4, uiOutput("colour"),uiOutput("pointsize"))
+                     #column(4, uiOutput("facet_col"),uiOutput("facet_row")),
+                     #column (4, uiOutput("pointsize"),uiOutput("fill"))
+                   )
+                 ),
+                 
+                 
+                 
+                 
+                 #regresion cuantilica
+                 conditionalPanel(
+                   condition = "input.showrqss" , 
+                   
+                   fluidRow(
+                     column(12,hr()),
+                     column(4,
+                            checkboxInput('Tauvalue', 'Cuantiles dinámicos', value = FALSE),
+                            h5("Preset Quantiles"),
+                            checkboxInput('superior', '95%'),
+                            checkboxInput('noventa', '90%'),
+                            checkboxInput('mitad', '50%', value = FALSE),
+                            checkboxInput('diez', '10%'),
+                            checkboxInput('inferior', '5%')
+                     ),
+                     column(4,
+                            sliderInput("Tau", label = "Valor cuantil dinámico",
+                                        min = 0, max = 1, value = 0.5, step = 0.01) 
+                     ),
+                     column(4,
+                            checkboxInput('ignorecolqr', 'Ignorar color'),
+                          #  checkboxInput('ignoregroupqr', 'Ignorar grupo',value = TRUE),
+                            checkboxInput('hidedynamic', 'Esconder cuantil dinamico'),
+                            selectInput('colqr', label ='Color', choices=colors(),multiple=FALSE, selectize=TRUE,selected="black")
+                     )
+                     
+                   )#fluidrow
+                 )
+        ),#tabPanel1
+        
+        tabPanel("Gráficas de la variable X",
+                 h3("Histograma de la variable X "),
+                 plotOutput("histplot"),
+                 h3("Diagrama de caja"),
+                 plotOutput("boxplot")
+                 
+                 ),
+        
+        tabPanel("Descarga",
+                 h3("Descarga de la gráfica de dispersión"),
+                 selectInput(
+                   inputId = "downloadPlotType",
+                   label   = h5("Selecciona el tipo de archivo"),
+                   choices = list("PDF"  = "pdf","BMP"  = "bmp","JPEG" = "jpeg","PNG"  = "png")),
+                 
+                 # Dimensiones de la imagen
+                 h5(HTML("Selecciona las dimensiones de la imagen<br>(unidades son pulgadas para PDF, pixeles para otrosformatos)")),
+                 numericInput(
+                   inputId = "downloadPlotHeight",label = "Altura (pulgadas)",value = 7,min = 1,max = 100),
+                 numericInput(
+                   inputId = "downloadPlotWidth",label = "Ancho (pulgadas)",value = 7,min = 1,max = 100),
+                 # Nombre del archivo
+                 textInput(
+                   inputId = "downloadPlotFileName",
+                   label = h5("Escribe el nombre del archivo")),
+                 
+                 # Descargar con botón
+                 downloadButton(
+                   outputId = "downloadPlot", 
+                   label    = "Descarga gráfica")
+                 
+        )#tabPanel2
+      )#tabsetPanel
+    )#mainPanel
+  )#sidebarLayout
+)#fluidPage
+)
